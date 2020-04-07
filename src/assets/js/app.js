3b6c4750f2ab8930f4054cf0d3c6ac37206df99d
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2020-04-07 16:34:01 +0300

    slider is over

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,2 +1,10 @@
+// Подключенные библиотеки!
+
 //= ../../../node_modules/bootstrap/js/dist/util.js
 //= ../../../node_modules/bootstrap/js/dist/modal.js
+//= ../../../node_modules/slick-carousel/slick/slick.js
+
+// Самописный КОД!
+$(function(){
+    //= components/slick.js
+})
